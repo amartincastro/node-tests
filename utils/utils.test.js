a960bce1888a55712a5a5c30469725ac55dced00
amartincastro <castroarielm@gmail.com>
2018-08-16 13:26:29 -0700

    import expect library and replace test logic with assertions

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,17 +1,24 @@
+const expect = require('expect');
+
 const utils = require('./utils');
 
 it('should add two numbers', () => {
   var res = utils.add(33, 11);
 
-  if (res !== 44) {
-    throw new Error(`Expected 44, but got ${res}.`)
-  }
+  expect(res).toBe(44).toBeA('number');
+
+  // if (res !== 44) {
+  //   throw new Error(`Expected 44, but got ${res}.`)
+  // }
 });
 
 it('should square a number', () => {
   var res = utils.square(3);
 
-  if (res !== 9) {
-    throw new Error(`Expected 9, but got ${res}.`);
-  }
+  expect(res).toBe(9).toBeA('number');
+});
+
+it('should expect some values', () => {
+  // expect(12).toNotBe(11);
+  expect({name: 'Ariel'}).toEqual({name: 'Ariel'});
 });
