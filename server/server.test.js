98e2a0e2ad3d221e0d7e5e03a128f03af05e6f24
amartincastro <castroarielm@gmail.com>
2018-08-23 11:25:01 -0700

    use expect and supertest to test 404 page response

--- server/server.test.js
+++ server/server.test.js
@@ -1,10 +1,16 @@
 const request = require('supertest');
+const expect = require('expect');
 
 var app = require('./server.js').app;
 
 it('should return hello world response', (done) => {
   request(app)
     .get('/')
-    .expect('Hello world!')
+    .expect(404)
+    .expect((res) => {
+      expect(res.body).toInclude({
+        error: 'Page not found.'
+      });
+    })
     .end(done);
 });
